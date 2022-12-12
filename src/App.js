13b35cd12dd6dd829297c7a5ed852affaeed0350
muhaiminul-islam-bjit <muhaimiul.islam@bjitgroup.com>
2022-12-12 17:08:04 +0600

    aded videos slice

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 // import Home from "./pages/Home";
 import Video from "./pages/Video";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 function App() {
-    return <Video />;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/videos/:videoId" element={<Video />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
